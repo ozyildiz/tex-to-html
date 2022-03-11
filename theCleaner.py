a5a1f014f3241b3c874182a8cbcaf0e71cc70d5c
zined <ozyildizz@gmail.com>
2022-03-11 19:47:33 +0100

    wrote collectThree, which does a better job than collectTwo

--- theCleaner.py
+++ theCleaner.py
@@ -67,42 +67,99 @@ def curlyGrab(string, Wrapper='{'):
 # The second case is one where brackets do contain other brackets. Here, we pop
 # the first and the last item in d. I can't imagine any other cases.
 def curlyTwo(string, wo='{', wc='}'):
-    n = 0
-    j = 0
-    k = 0
-    d = [] 
-    l = []
+    n = j = k = 0
+    d = []
+    l = [] 
+    tmp = []
     splat = [c for c in string]
     for i in range(len(splat)):
         if splat[i] == wo:
             n+=1
-            d.append([n,'o',i])
+            d.append([n,'o',i+1])
         elif splat[i] == wc:
             d.append([n,'c',i]) 
             n-=1
-    print(d)
-    while j < len(d):
-        if d[j][0] == d[j+1][0] and d[j][1]=='o' and d[j+1][1]=='c':
-            l.append([d[j][2],d[j+1][2]])
-            d.remove(d[j+1]) # this has to come first, otherwise, you end up trying
-            # to get the second element of a singleton list
-            d.remove(d[j]) 
-        j+=1 
-    while k < len(d):
-        print(k)
-        l.append([d[0][2],d[-1][2]])
-        d.remove(d[-1])
-        d.remove(d[0])
-        k+=1
-    print(d)
-    return(l)
+    # print(d)
+    if len(d)%2 == 0: # originally, this didn't handle cases where there were no
+        # matching brackets on a given line, e.g., when a bracket closes a line
+        # and it's matched some lines below. To circumvent, for now, make sure
+        # that the list contains an even number of elements.
+        while j < len(d) - 1: # minus one here because we will never check the last
+#            print(j, j+1, len(d))
+            # element of the list (should always be a closing bracket).
+            if d[j][0] == d[j+1][0] and d[j][1]=='o' and d[j+1][1]=='c':
+                l.append([d[j][2],d[j+1][2]])
+                print(d)
+                d.remove(d[j+1]) # this has to come first, otherwise, you end up trying
+                # to get the second element of a singleton list
+                d.remove(d[j]) 
+            j+=1 
+        while k < len(d):
+            l.append([d[0][2],d[-1][2]])
+            print(d)
+            d.remove(d[-1])
+            d.remove(d[0])
+            k+=1
+        for line in l:
+            tmp.append(string[line[0]:line[1]])
+    else:
+        tmp.append("no matching bracket")
+    return(l, tmp)
+# Right now, this isn't doing what it's supposed to. Cf. test7 below
 
-test = "\\textbf{sdf}"
-test2 = "\\textbf{it\\textit{al}ic}"
-test3 = "\\textbf{test}\\testit{adsf}"
-test4 = "\\textbf{test}\\testit{\textbf{\textit{adsf}}adsf}\textit{adsf}"
 
-print(curlyTwo(test4))
+# test7 = "\ref{ex:fsg1premise} \ref{ex:fsg1conclus ion} \emph{Tuesday} \emph{Peter}"# instead of on \emph{Tuesday}.  If the results of the original test and this additional test differ, then t he predicate is focus sensitive. Please refer to the  \href{https://docs.goo gle.com/document/d/1Z6OEynS_sgjbz43gjcR92ba0C1zelicaOSk9DYtCids}{predicate s pecific notes} for further details about the predicates involved and the add itional tests for them. For most predicates, it should be quite clear whethe r they are UE when the embedded clause does not contain focus, but this may not be the case for some other predicates. As a rule thumb, if you suspect t hat a predicate might be non-UE when the embedded clause does not contain fo cus, then please run the additional test just to be safe."
+# test7 = "\ref{ex} \ref{to} \emph{lo}"
+#test0 = "\\textbf{sdf}"
+#test1 = "\\textbf{it\\textit{al}ic}"
+#test2 = "\\textbf{test}\\testit{adsf}"
+#test3 = "\\textbf{test}\\testit{adsf}\\textbf{asdf}"
+#test4 = "\\textbf{bold}\\testsl{\\textbf{\\textit{slanted and bold and italic}}bold and slanted}\\textit{italic}"
+#test5 = "\\renewcommand{\\firstrefdash}{}"
+#test6 = "\newcolumntype{R}[2]{%"
+#
+# print(curlyTwo(test7))
+
+# increment upon encountering ob, keep looking until you find matching cb,
+# extract string, feed leftover to the same function 
+# if running in a loop, you need to specify 'collect=[]' Otherwise, the list
+# will just keep growing.
+def curlyThree(ligne, collect=[], ob='{', cb='}'): 
+    # we also need to handle the \{, \} case.
+    tmp = []
+    n = 0
+    spligne = [char for char in ligne]
+    beg = end = 0
+    if ob not in spligne and cb not in spligne: # the no brackets case
+        print(ligne)
+    else:
+        for i in range(len(spligne)):
+            if spligne[i] == ob:
+                n += 1
+                if n == 1:
+                    beg = i
+            if spligne[i] == cb: 
+                n -= 1 
+                if n == 0:
+                    end = i
+                    tmp.append(''.join(ligne[beg+1:end])) 
+                    collect.append(''.join(ligne[beg+1:end])) 
+                    #print(tmp)
+        for j in range(len(tmp)):
+            if ob in tmp[j] and cb in tmp[j]:
+                tmp.append(curlyThree(tmp[j], collect))
+    return(collect)
+
+#nob = "testing"
+test="{{1re}1{2re}2op{3eft{4n{6asdf}6}4ight}3ot{5nd}5}"
+test2 = "{out{in}out}"
+test3 = "{out{a{i{{}}n}a}out}"
+test4 = "{s}{d}{{x}}"
+T = [test, test2, test3]
+for t in T:
+    print(curlyThree(t,[])) 
+
+
 
 # This will take a tag (e.g., b) and some content, and output that content in
 # those tags (e.g., makeTag("i","test") returns "<i>test</i>")
@@ -158,6 +215,8 @@ def noComment(liste):
             indexOfPercent = [char for char in liste[i]].index('%')
             tempList.append(liste[i][:indexOfPercent])
     return(tempList)
+# This will handle things like a comment character ending a line.
+
 
 
 # This gets rid of all \n and \ts, as well as leading and trailing whitespace.
